674e6570fc2ea80b9a388adb8a4b84bf2a53598b
Thiago Cedran Santos <thiagocdn@TCedran-Dell>
2022-10-23 22:14:40 -0300

    Added ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
