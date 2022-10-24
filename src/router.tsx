f0db53a7b29e7a97d68c9a34c233c8d805ffd17a
Thiago Cedran Santos <thiagocdn@TCedran-Dell>
2022-10-24 18:06:41 -0300

    Added router

---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import { History } from './pages/history'
+import { Home } from './pages/home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
+  )
+}
