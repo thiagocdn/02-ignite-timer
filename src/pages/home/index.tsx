6a9ab0405e3e653febe34d9ea9defd8174192e40
Thiago Cedran Santos <thiagocdn@TCedran-Dell>
2022-10-24 20:19:19 -0300

    Finished timer page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -15,13 +15,25 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para seu projeto"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 01" />
+            <option value="Projeto 02" />
+            <option value="Projeto 03" />
+          </datalist>
 
           <label htmlFor="durante">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
