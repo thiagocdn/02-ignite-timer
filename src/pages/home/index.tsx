e67528c7c15b0b608d5ec63e13729a148f155849
Thiago Cedran Santos <thiagocdn@TCedran-Dell>
2022-10-25 20:43:57 -0300

    Added reset function from react-hook-form

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -24,20 +24,20 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
       minutesAmount: 0,
     },
   })
+  const task = watch('task')
+  const isSubmitDisabled = !task
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log('data', data)
+    reset()
   }
-
-  const task = watch('task')
-  const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
