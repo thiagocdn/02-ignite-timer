1c12d6c40e3922321b183dc0b17d4eaa8f56193d
Thiago Cedran Santos <thiagocdn@TCedran-Dell>
2022-10-30 21:35:47 -0300

    Added cycle interruption

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -86,11 +86,12 @@ export const Separator = styled.div`
   justify-content: center;
 `
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
   border-radius: 8px;
+  color: ${(props) => props.theme['gray-100']};
 
   display: flex;
   align-items: center;
@@ -102,15 +103,25 @@ export const StartCountdownButton = styled.button`
 
   cursor: pointer;
 
-  background: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
-
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
 
   &:hover:not(:disabled) {
     background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:hover:not(:disabled) {
+    background: ${(props) => props.theme['red-700']};
   }
 `
